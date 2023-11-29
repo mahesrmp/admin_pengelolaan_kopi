de7ab77410e92709bc8ade6f297c47dae95e14e9
mahesrmp <richmenpanjaitan01@gmail.com>
2023-11-29 23:35:27 +0700

    perbaiki fungsi pengajuan

--- app/Http/API/PengajuanController.php
+++ app/Http/API/PengajuanController.php
@@ -5,33 +5,44 @@ namespace App\Http\API;
 use App\Models\Pengajuan;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PengajuanController extends Controller
 {
     public function tambahData(Request $request)
     {
-        $request->validate([
-            'foto_ktp' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'foto_selfie' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'deskripsi_pengalaman' => 'required|string',
-            'foto_sertifikat' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
-
-        // $fotoKtpPath = $request->file('foto_ktp')->storeAs('public/pengajuan', 'foto_ktp_' . time() . '.' . $request->file('foto_ktp')->extension());
-        // $fotoSelfiePath = $request->file('foto_selfie')->storeAs('public/pengajuan', 'foto_selfie_' . time() . '.' . $request->file('foto_selfie')->extension());
-        // $fotoSertifikatPath = $request->file('foto_sertifikat')->storeAs('public/pengajuan', 'foto_sertifikat_' . time() . '.' . $request->file('foto_sertifikat')->extension());
-
-        $fotoKtpPath = $request->file('foto_ktp')->store('pengajuanimage', 'public');
-        $fotoSelfiePath = $request->file('foto_selfie')->store('pengajuanimage', 'public');
-        $fotoSertifikatPath = $request->file('foto_sertifikat')->store('pengajuanimage', 'public');
-
-        $pengajuan = Pengajuan::create([
-            'foto_ktp' => $fotoKtpPath,
-            'foto_selfie' => $fotoSelfiePath,
-            'deskripsi_pengalaman' => $request->deskripsi_pengalaman,
-            'foto_sertifikat' => $fotoSertifikatPath,
-        ]);
-
-        return response()->json(['message' => 'Data berhasil diajukan'], 200);
-    }
+        try {
+            $request->validate([
+                'foto_ktp' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'foto_selfie' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'deskripsi_pengalaman' => 'required|string',
+                'foto_sertifikat' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+    
+            // Get the ID of the logged-in user
+            $petaniId = Auth::id();
+    
+            $fotoKtpPath = $request->file('foto_ktp')->store('pengajuanimage', 'public');
+            $fotoSelfiePath = $request->file('foto_selfie')->store('pengajuanimage', 'public');
+            $fotoSertifikatPath = $request->file('foto_sertifikat')->store('pengajuanimage', 'public');
+    
+            $pengajuan = Pengajuan::create([
+                'foto_ktp' => $fotoKtpPath,
+                'foto_selfie' => $fotoSelfiePath,
+                'deskripsi_pengalaman' => $request->deskripsi_pengalaman,
+                'foto_sertifikat' => $fotoSertifikatPath,
+                'petani_id' => $petaniId, // Save the user ID in the petani_id column
+            ]);
+    
+            if ($pengajuan) {
+                // Data added successfully
+                return response()->json(['message' => 'Data berhasil diajukan', 'status' => 'success'], 200);
+            } else {
+                // Failed to add data
+                return response()->json(['message' => 'Gagal menambahkan data', 'status' => 'error', 'error' => 'Failed to save data to the database'], 500);
+            }
+        } catch (\Exception $e) {
+            // Handle other exceptions
+            return response()->json(['message' => 'Gagal menambahkan data', 'status' => 'error', 'error' => $e->getMessage()], 500);
+        }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,26 +16,33 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // Hapus semua data dari tabel users sebelum menambahkan data dummy
-        DB::table('users')->truncate();
+        // // Hapus semua data dari tabel users sebelum menambahkan data dummy
+        // DB::table('users')->truncate();
 
-        // Tambahkan data dummy
-        DB::table('users')->insert([
-            'name' => 'John Doe',
-            'email' => 'john@gmail.com',
-            'password' => Hash::make('password123'),
-        ]);
+        // // Tambahkan data dummy
+        // DB::table('users')->insert([
+        //     'name' => 'John Doe',
+        //     'email' => 'john@gmail.com',
+        //     'password' => Hash::make('password123'),
+        // ]);
 
-        DB::table('users')->insert([
-            'name' => 'Jane Doe',
-            'email' => 'jane@gmail.com',
-            'password' => Hash::make('password456'),
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'Jane Doe',
+        //     'email' => 'jane@gmail.com',
+        //     'password' => Hash::make('password456'),
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
+        //     'name' => 'gilang',
+        //     'email' => 'gilang@gmail.com',
+        //     'password' => Hash::make('gilanglang1'),
+        // ]);
+
+        \App\Models\User::factory()->create([
             'name' => 'gilang',
             'email' => 'gilang@gmail.com',
             'password' => Hash::make('gilanglang1'),
+
         ]);
     }
 }
